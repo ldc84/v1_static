c2be5f0a78c0f8738a48da9300ac2b3ab0bcee26
ldc84 <goodldc84@gmail.com>
2019-07-11 17:16:14 +0900

    validator.one :: 깃허브 페이지에 반영됨. Published to Github pages

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,53 @@
 // 설치후 작업
-this.addEventListener('install', function(event){
-  console.log('설치 완료', event);
+var PRECACHE = 'v1_static';
+var RUNTIME = 'runtime';
+
+// list the files you want cached by the service worker
+PRECACHE_URLS = [
+  'index.html',
+  './',
+  './css/style.css',
+  './js/js.js'
+];
+
+
+// the rest below handles the installing and caching
+self.addEventListener('install', event => {
+  event.waitUntil(
+     caches.open(PRECACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
